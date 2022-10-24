6754fa39f0f8c9645a05cc86581361bcd4ac2271
ujhessie_ <juniorrodrigues4502@gmail.com>
2022-10-24 06:28:12 -0300

    aaa

--- js/login.js
+++ js/login.js
@@ -1,6 +1,9 @@
 var  inputLogin = document.querySelector('#iinputLogin')
 var buttonPlay = document.querySelector('#ibuttonPlay')
+var form = document.querySelector('#iform')
 
+
+// validação de login
 const validateLogin = ({target}) => {
     if (target.value.length < 3 ) {
         console.log('iiii pegou nao po')
@@ -12,4 +15,23 @@ const validateLogin = ({target}) => {
     }
 }
 
+
+// retirar funcionamento de recarregar de submmit
+const handleSubmit = (event) => {
+    event.preventDefault() // Bloqueia funcionamento de recarregar de um formulário
+    console.log(inputLogin.value)
+    localStorage.setItem("player", inputLogin.value)
+    window.location = 'pages/game.html'
+}
+
+
+
+
+//salvar em localstorage
+
+
+// retirar funcionamento de recarregar de submmit
+form.addEventListener('submit', handleSubmit)
+
+// Validação de login
 inputLogin.addEventListener('input', validateLogin)
